9015ff8f308b0ac17e0fac505b9c05c8814002d9
Ruoxi Wang <thuwrx10@gmail.com>
2017-12-09 20:31:09 +0800

    mdiCodeExe: Utilize python's with statement to do mode returning.

--- mdiCodeExec.py
+++ mdiCodeExec.py
@@ -7,8 +7,6 @@ import logging
 class mdiCodeExec:
 
     def exe(self, code):
-        if not self.sessionStarted:
-            self.sessionStart()
         self.command.mdi(code)
         return self.command.wait_complete(self.timeout)
 
@@ -30,6 +28,17 @@ class mdiCodeExec:
         self.logger = logging.getLogger(__name__)
         self.sessionStarted = False
         self.timeout = timeout
+        self.sessionStart()
+        self.logger.debug("Started MDI session, previous mode "
+                          "is {}".format(self.prevMode))
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_value, traceback):
+        self.sessionFinish()
+        self.logger.debug("Finished MDI session, "
+                          "returned mode {}".format(self.prevMode))
 
 
 # Code for testing
@@ -37,28 +46,26 @@ if __name__ == "__main__":
     logging.basicConfig(level=logging.DEBUG)
     logger = logging.getLogger(__name__)
 
-    mdi = mdiCodeExec()
+    c = linuxcnc.command()
+    c.mode(linuxcnc.MODE_MANUAL)
+    logger.debug("LinuxCNC mode switched to MANUAL to "
+                 "test mode returning.")
 
-    mdi.command.mode(linuxcnc.MODE_MANUAL)
-    logger.debug("LinuxCNC mode switched to MANUAL to test mode returning.")
+    with mdiCodeExec() as mdi:
 
-    mdi.sessionStart()
-    logger.debug("Started MDI session.")
+        mdi.exe("G91")  # Incremental distance mode
 
-    mdi.exe("G91")  # Incremental distance mode
+        assert mdi.exe("G38.2 X0.1 F50") == linuxcnc.RCS_ERROR
+        logger.debug("Non-tripped G38.2 probing returned RCS_ERROR correctly.")
 
-    assert mdi.exe("G38.2 X0.1 F50") == linuxcnc.RCS_ERROR
-    logger.debug("Non-tripped G38.2 probing returned RCS_ERROR correctly.")
+        assert mdi.exe("G38.3 X0.1 F50") == linuxcnc.RCS_DONE
+        logger.debug("Non-tripped G38.3 probing returned RCS_DONE correctly.")
 
-    assert mdi.exe("G38.3 X0.1 F50") == linuxcnc.RCS_DONE
-    logger.debug("Non-tripped G38.3 probing returned RCS_DONE correctly.")
+        mdi.timeout = 1.0
+        assert mdi.exe("G1 X2 F60") == -1  # Timeout
+        mdi.timeout = 30.0
 
-    mdi.timeout = 1.0
-    assert mdi.exe("G1 X2 F60") == -1  # Timeout
-    mdi.timeout = 30.0
+        mdi.exe("G90")
 
-    mdi.exe("G90")
-
-    mdi.sessionFinish()
     logger.debug("Successfully finished the MDI sesion. LinuxCNC should be in "
                  "MANUAL mode now.")
